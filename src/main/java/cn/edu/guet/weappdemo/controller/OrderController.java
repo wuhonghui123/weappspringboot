b2fe00c2f561f3ddf803675f2c8f821c771d6b76
iseven <hezs1026@qq.com>
2022-07-28 21:16:33 +0800

    实现订单增、删、查操作

--- src/main/java/cn/edu/guet/weappdemo/controller/OrderController.java
+++ src/main/java/cn/edu/guet/weappdemo/controller/OrderController.java
@@ -1,22 +1,30 @@
 package cn.edu.guet.weappdemo.controller;
 
+import cn.edu.guet.weappdemo.bean.Order;
 import cn.edu.guet.weappdemo.http.HttpResult;
+import cn.edu.guet.weappdemo.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: wuhonghui
  * @Date: 2022/07/25/19:01
  */
+@RestController
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
     @GetMapping("/order/list")//获取用户订单列表
-    private HttpResult getOrderList(){
-        return HttpResult.ok();
+    private HttpResult getOrderList(String user_id){
+        return HttpResult.ok(orderService.getOrderList(user_id));
     }
 
     @PostMapping("/order/delete")//删除用户所选订单
-    public HttpResult deleteOrder(){
-        return HttpResult.ok();
+    public HttpResult deleteOrder(String id,String pay_type){
+        return HttpResult.ok(orderService.deleteOrder(id, pay_type));
     }
 
     @PostMapping("/order/update_type")//更新订单状态，确认送达
@@ -25,7 +33,7 @@ public class OrderController {
     }
 
     @PostMapping("/order/addorder")//生成订单
-    public HttpResult addOrder(){
-        return HttpResult.ok();
+    public HttpResult addOrder(Order order){
+        return HttpResult.ok(orderService.addOrder(order));
     }
 }
