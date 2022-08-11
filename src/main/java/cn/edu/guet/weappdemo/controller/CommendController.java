1dbe9456295846cdf409226562c1122fcc8f2be6
whh <2695803476@qq.com>
2022-08-11 15:31:13 +0800

    1

--- src/main/java/cn/edu/guet/weappdemo/controller/CommendController.java
+++ src/main/java/cn/edu/guet/weappdemo/controller/CommendController.java
@@ -1,12 +1,36 @@
 package cn.edu.guet.weappdemo.controller;
 
+import cn.edu.guet.weappdemo.bean.Commend;
+import cn.edu.guet.weappdemo.http.HttpResult;
+import cn.edu.guet.weappdemo.service.CommendService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * @Author: wuhonghui
- * @Date: 2022/07/26/1:03
+ * @Date: 2022/07/26/9:36
  */
-//评论控制器，用户评论与追加评论
-@RestController
+//商家追加评论
 public class CommendController {
+
+    @Autowired
+    private CommendService commendService;
+    @PostMapping("/commend/list")//获取评论列表
+    public HttpResult getCommend(){
+        return HttpResult.ok(commendService.getCommend());
+    }
+    @PostMapping("/commend/add")//添加评论
+    public HttpResult addCommend(@RequestBody Commend commend){
+
+        return HttpResult.ok(commendService.addCommend(commend));
+    }
+    @PostMapping("/commend/update")//更新评论
+    public HttpResult updateCommend(@RequestBody Commend commend){
+
+        return HttpResult.ok(commendService.updateCommend(commend));
+    }
+
 }
