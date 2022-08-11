149016942d7ca5c285e400c930e23ab5a99fc882
whh <2695803476@qq.com>
2022-08-11 15:45:25 +0800

    1

--- src/main/java/cn/edu/guet/weappdemo/controller/CommendController.java
+++ src/main/java/cn/edu/guet/weappdemo/controller/CommendController.java
@@ -4,6 +4,7 @@ import cn.edu.guet.weappdemo.bean.Commend;
 import cn.edu.guet.weappdemo.http.HttpResult;
 import cn.edu.guet.weappdemo.service.CommendService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class CommendController {
 
     @Autowired
     private CommendService commendService;
-    @PostMapping("/commend/list")//获取评论列表
+    @GetMapping("/commend/list")//获取评论列表
     public HttpResult getCommend(){
         return HttpResult.ok(commendService.getCommend());
     }
