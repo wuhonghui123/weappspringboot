46a70ef856f38a0c167789a2a681ca8380c62efd
iseven <hezs1026@qq.com>
2022-08-12 10:00:42 +0800

    实现订单增、删、查操作

--- src/main/java/cn/edu/guet/weappdemo/controller/OrderController.java
+++ src/main/java/cn/edu/guet/weappdemo/controller/OrderController.java
@@ -3,23 +3,26 @@ package cn.edu.guet.weappdemo.controller;
 import cn.edu.guet.weappdemo.bean.Order;
 import cn.edu.guet.weappdemo.http.HttpResult;
 import cn.edu.guet.weappdemo.service.OrderService;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: wuhonghui
  * @Date: 2022/07/25/19:01
  */
+
 @RestController
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
     @GetMapping("/order/list")//获取用户订单列表
-    private HttpResult getOrderList(String user_id){
-        return HttpResult.ok(orderService.getOrderList(user_id));
+    private HttpResult getOrderList(String id,String user_id,String order_type,String pay_type,String create_time){
+        return HttpResult.ok(orderService.getOrderList(id,user_id,order_type,pay_type,create_time));
     }
 
     @PostMapping("/order/delete")//删除用户所选订单
@@ -27,13 +30,19 @@ public class OrderController {
         return HttpResult.ok(orderService.deleteOrder(id, pay_type));
     }
 
-    @PostMapping("/order/update_type")//更新订单状态，确认送达
-    public HttpResult updateOrderType(){
-        return HttpResult.ok();
+    @GetMapping("/order/update_type")//更新订单状态，确认送达
+    public HttpResult updateOrderType(String order_id,String user_id,String order_type){
+        System.out.println(orderService.updateOrder(order_id, user_id, order_type)+"######"+order_type+order_id);
+        return HttpResult.ok(orderService.getOrderList(null,user_id,null,null,null));
     }
 
     @PostMapping("/order/addorder")//生成订单
     public HttpResult addOrder(Order order){
         return HttpResult.ok(orderService.addOrder(order));
+    }
+    @GetMapping("/order/search")
+    public HttpResult orderUserList(String order_id,String user_id){
+        System.out.println("结果看这"+orderService.orderUserList(order_id,user_id));
+        return HttpResult.ok(orderService.orderUserList(order_id,user_id));
     }
 }
--- src/main/java/cn/edu/guet/weappdemo/service/impl/OrderServiceImpl.java
+++ src/main/java/cn/edu/guet/weappdemo/service/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.edu.guet.weappdemo.service.impl;
 
 import cn.edu.guet.weappdemo.bean.Order;
+import cn.edu.guet.weappdemo.bean.OrderUser;
 import cn.edu.guet.weappdemo.mapper.OrderMapper;
 import cn.edu.guet.weappdemo.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,9 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderMapper orderMapper;
     @Override
-    public List<Order> getOrderList(String user_id) {
-        return orderMapper.getOrderList(user_id);
+    public List<Order> getOrderList(String id,String user_id,String order_type,String pay_type,String create_time) {
+        return orderMapper.getOrderList(id,user_id,order_type,pay_type,create_time);
     }
-
     @Override
     public int addOrder(Order order) {
         return orderMapper.addOrder(order);
@@ -25,5 +25,15 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public int deleteOrder(String id, String pay_type) {
         return orderMapper.deleteOrder(id, pay_type);
+    }
+
+    @Override
+    public List<OrderUser> orderUserList(String order_id, String user_id) {
+        return orderMapper.orderUserList(order_id, user_id);
+    }
+
+    @Override
+    public int updateOrder(String order_id, String user_id, String order_type) {
+        return orderMapper.updateOrder(order_id, user_id, order_type);
     }
 }
